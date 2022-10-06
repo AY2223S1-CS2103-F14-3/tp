41f06ba1d839e6c2d94a19e017e5b3bc58579c33
Chee Jer Hong <e0407708@u.nus.edu>
2022-10-07 00:18:45 +0800

    Fix checkstyle error for test

--- src/test/java/seedu/address/model/application/DateTest.java
+++ src/test/java/seedu/address/model/application/DateTest.java
@@ -1,11 +1,10 @@
 package seedu.address.model.application;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.model.application.Date;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 public class DateTest {
 
